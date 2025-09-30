54d64573c911db8e60ff4276464f2c950b943565
Chidi Mathaias <chidimathayas@gmail.com>
2025-09-30 14:22:02 +0100

    sign

--- app/_components/Footer.tsx
+++ app/_components/Footer.tsx
@@ -7,12 +7,12 @@ import krea from "@/public/kreaLogo.png";
 const Footer = () => {
   return (
     <div className="flex items-center justify-between w-full px-2 py-5 bg-black/95 md:px-6">
-      <div className="flex justify-center items-center gap-3">
+      <div className="flex flex-col md:flex-row  justify-center items-center gap-3">
         <Image src={krea} quality={90} width="35" height="35" alt="krea logo" />
-        <p className='text-white text-xl'>Krea AI</p>
+        <p className="text-white text-xl">Krea AI</p>
       </div>
-      <div className="flex justify-center items-center gap-3">
-                <p className='text-white text-xl'>Created by</p>
+      <div className="flex flex-col md:flex-row justify-center items-center gap-3">
+        <p className="text-white text-xl">Created by</p>
 
         <Image
           src={mobbin}
@@ -21,7 +21,6 @@ const Footer = () => {
           height="200"
           alt="mobbin logo"
         />
-
       </div>
     </div>
   );
